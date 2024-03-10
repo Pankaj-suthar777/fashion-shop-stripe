e461a110ed8b9a6611377c1feaf75631020d72d0
pankaj suthar <146710838+Pankaj-suthar777@users.noreply.github.com>
2024-03-11 00:28:28 +0530

    Update helper.js

--- client/src/helper.js
+++ client/src/helper.js
@@ -2,7 +2,8 @@ export const isInCart = (product, cartItems) => {
   return cartItems.find((item) => item.id === product.id);
 };
 
-const API = "http://localhost:5000";
+//const API = "http://localhost:5000";
+const API = "https://fashion-shop-stripe.onrender.com"
 
 export async function fetchFromAPI(endPoint, opts) {
   const { method, body } = { method: "POST", body: null, ...opts };
